827ef74e8897e51cbcf420b6640178aa4926f022
Brandon <Brandon.Kamaka@afit.edu>
2019-02-26 12:18:01 -0500

    Initial commit

---
+++ biffPW.py
@@ -0,0 +1,34 @@
+import itertools
+
+
+def main():
+    base_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    permutations = itertools.permutations(base_string, 3)
+
+    with open('biff_passwords.txt', 'a+') as f:
+        for p in permutations:
+            for i in range(2187):
+                i_tern = convert_to_base(i, 3)
+                candidate = f'{i_tern:>07}'
+
+                for j in range(3):
+                    candidate = candidate.replace(str(j), p[j])
+
+                f.write(candidate)
+                f.write('\n')
+                print(f'Processed password: {candidate}\n')
+
+
+def convert_to_base(original, new_base):
+    # Pulled from https://stackoverflow.com/questions/34559663/convert-decimal-to-ternarybase3-in-python
+    if original == 0:
+        return '0'
+    nums = []
+    while original:
+        original, r = divmod(original, new_base)
+        nums.append(str(r))
+    return ''.join(reversed(nums))
+
+
+if __name__ == '__main__':
+    main()
